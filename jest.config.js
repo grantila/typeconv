21481013e371bf2aa5deda3112077ac9ae957d77
Gustaf Räntilä <g.rantila@gmail.com>
2023-06-04 10:09:05 +0200

    test(jest): Renamed test files

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 export default {
 	resolver: "ts-jest-resolver",
 	testEnvironment: 'node',
-	testMatch: [ '<rootDir>/lib/**/*.spec.ts', '<rootDir>/lib/**/*.test.ts' ],
+	testMatch: [ '<rootDir>/lib/**/*.test.ts' ],
 	collectCoverageFrom: [ '<rootDir>/lib/**' ],
 	coveragePathIgnorePatterns: [ '/node_modules/', '/__snapshots__/', '/bin/' ],
 	coverageReporters: [ 'lcov', 'text', 'html' ],
